c9fa24e6c4c99706384f6e9050c9756b756c6d34
Gregor Aisch <gregor.aisch@nytimes.com>
2016-07-28 00:05:37 +0200

    rect style

--- convert artboards to rectangles.jsx
+++ convert artboards to rectangles.jsx
@@ -1,75 +1,43 @@
 #include ".scripts/polyfills.jsx"
 #include ".scripts/underscore.jsx"
+#include ".scripts/utils.jsx"
 
 // put code in here
 var doc = app.activeDocument;
 var artboards = doc.artboards;
 
-function getColor(num) {
-    // var num = $.colorPicker();
-    // num >>>= 0;
-    // var b = num & 0xFF,
-    //     g = (num & 0xFF00) >>> 8,
-    //     r = (num & 0xFF0000) >>> 16,
-    //     a = ( (num & 0xFF000000) >>> 24 ) / 255 ;
-    var r = (num >> 16) & 255;
-    var g = (num >> 8) & 255;
-    var b = num & 255;
-    var col = new RGBColor();
-    col.red = r;
-    col.green = g;
-    col.blue = b;
-    return col;
-}
-
-var newRect = function(x, y, width, height) {
-    var l = 0;
-    var t = 1;
-    var r = 2;
-    var b = 3;
-
-    var rect = [];
-
-    rect[l] = x;
-    rect[t] = -y;
-    rect[r] = width + x;
-    rect[b] = -(height - rect[t]);
-
-    return rect;
-};
-
 
 var ignore = [],
-  convert = [];
+    convert = [];
 
 _.each(artboards, function(ab) {
-  if (ab.name.charAt(0) == '-') ignore.push(ab);
-  else convert.push(ab);
-})
+    if (ab.name.charAt(0) == '-') ignore.push(ab);
+    else convert.push(ab);
+});
 
 if (!ignore.length) {
-  // make a fake artboard
-  ab = artboards.add(artboards[0].artboardRect);
-  ab.name = "-- ignore me --";
-  ab.artboardRect = newRect(-600, -600, 10, 10);
+    // make a fake artboard
+    ab = artboards.add(artboards[0].artboardRect);
+    ab.name = "-- ignore me --";
+    ab.artboardRect = getRect(-600, -600, 10, 10);
 }
 
 // draw rect for each
 var toDelete = [];
 _.each(convert, function(ab) {
-    var top = ab.artboardRect[1];  
-    var left = ab.artboardRect[0];
-    var width = ab.artboardRect[2] - ab.artboardRect[0]; 
-    var height = ab.artboardRect[1] - ab.artboardRect[3];
-    var rect = doc.pathItems.rectangle (top, left, width, height);
+    var top = ab.artboardRect[1],
+        left = ab.artboardRect[0],
+        width = ab.artboardRect[2] - ab.artboardRect[0],
+        height = ab.artboardRect[1] - ab.artboardRect[3],
+        rect = doc.pathItems.rectangle (top, left, width, height);
     rect.filled = false;
-    rect.strokeColor = getColor(0x8a497e);
-    rect.strokeWidth = 30;
-    
+    rect.strokeColor = getColor(0xff00ff);
+    rect.strokeWidth = 10;
+    rect.opacity = 50;
     toDelete.push(ab.name);
 });
 
-// delete all the artboards
-for (i = 0; i < toDelete.length; i++) {
-  artboards.getByName(toDelete[i]).remove();
-}
+_.each(toDelete, function(name) {
+    artboards.getByName(name).remove();
+});
+
